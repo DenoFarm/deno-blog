92a9b714f1eb30a525f8933fa1978a5d50f8dfda
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2020-11-26 13:28:31 +0600

    empty form data bug fixed. use context.request.serverRequest to obtain form data

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-import { Application } from "https://deno.land/x/oak@v6.3.2/mod.ts";
-import { router } from "./routes/routes.js";
+import { Application, Router } from "https://deno.land/x/oak@v6.3.2/mod.ts";
+import { renderFileToString } from "https://deno.land/x/dejs@0.9.3/mod.ts";
+import { multiParser } from 'https://deno.land/x/multiparser@v2.0.3/mod.ts'
 
 
 const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
+const router = new Router();
+
+router
+  .get("/register", async (context) => {
+    context.response.body = await renderFileToString(
+      `${Deno.cwd()}/views/index.ejs`
+    );
+  })
+  .post("/register", async (context) => {
+    // const value = await context.request.serverRequest;
+    // // const username = value.get("username");
+    // console.log(`Value: ${JSON.stringify(value)}`);
+    // context.response.redirect("/register");
+    const form = JSON.stringify(await multiParser(context.request.serverRequest));
+    const parse = JSON.parse(form);
+    console.log(parse["fields"]["username"]);
+    context.response.redirect("/register")
+  });
+
 app.addEventListener('error', event => {
   console.log(event.error);
-})
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
 console.log("Server is running 🦕")
-app.listen({ port: 8000 });
+await app.listen({ port: 8000 });
--- utils/handlebars.js
+++
@@ -1,3 +0,0 @@
-import { Handlebars } from 'https://deno.land/x/handlebars@v0.5.0/mod.ts'
-
-export const handle = new Handlebars();
