82a599a255b1a6537ae485585706a123644fb2f9
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2020-11-27 23:44:50 +0600

    database dependencies added

--- dependencies.js
+++ dependencies.js
@@ -1,3 +1,8 @@
 export { Application, Router } from "https://deno.land/x/oak@v6.3.2/mod.ts";
 export { renderFileToString }  from "https://deno.land/x/dejs@0.9.3/mod.ts";
 export { multiParser }         from 'https://deno.land/x/multiparser@v2.0.3/mod.ts';
+
+// mongo database
+export { MongoClient } from "https://deno.land/x/mongo@v0.13.0/mod.ts"
+
+export { config }      from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
