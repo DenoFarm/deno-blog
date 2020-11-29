a5b1318032ebc0660730ac0c4f020ce8f0312e12
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2020-11-29 22:24:45 +0600

    Login Register pages added

--- server.js
+++ server.js
@@ -7,20 +7,32 @@ const app = new Application();
 const router = new Router();
 
 router
-  .get("/register", async (context) => {
+  .get("/", async (context) => {
     context.response.body = await renderFileToString(
       `${Deno.cwd()}/views/index.ejs`
     );
   })
+  .get("/register", async (context) => {
+    context.response.body = await renderFileToString(
+      `${Deno.cwd()}/views/pages/authorization/registration.ejs`
+    );
+  })
   .post("/register", async (context) => {
-    // const value = await context.request.serverRequest;
-    // // const username = value.get("username");
-    // console.log(`Value: ${JSON.stringify(value)}`);
-    // context.response.redirect("/register");
     const form = JSON.stringify(await multiParser(context.request.serverRequest));
     const parse = JSON.parse(form);
     console.log(parse["fields"]["username"]);
-    context.response.redirect("/register")
+    context.response.redirect("/")
+  })
+  .get("/login", async (context) => {
+    context.response.body = await renderFileToString(
+      `${Deno.cwd()}/views/pages/authorization/login.ejs`
+    );
+  })
+  .post("/login", async (context) => {
+    const form = JSON.stringify(await multiParser(context.request.serverRequest));
+    const parse = JSON.parse(form);
+    console.log(parse["fields"]["username"]);
+    context.response.redirect("/")
   });
 
 app.addEventListener('error', event => {
